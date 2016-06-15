3cecd3db6a9082a6b7471b4555553fd0dcc1fd95
elad <elad.tabak@ravellosystems.com>
2016-06-15 16:35:15 +0300

    refresh applications on controller init

--- js/ng-tutorial.js
+++ js/ng-tutorial.js
@@ -1,35 +1,6 @@
 angular.module("UiIntro", [])
 	.controller("AppsController", function($scope, $http) {
-		$scope.apps = [
-		               {
-		               	id: 1,
-		               	name: 'app1',
-		               	status: 'Running',
-		               	owner: 'Elad',
-		               	created: '1/1/2016'
-		               },
-		               {
-		               	id: 2,
-		               	name: 'app2',
-		               	status: 'Stopped',
-		               	owner: 'Yaniv',
-		               	created: '2/1/2016'
-		               },
-		               {
-		               	id: 3,
-		               	name: 'app3',
-		               	status: 'Error',
-		               	owner: 'Noam',
-		               	created: '3/1/2016'
-		               },
-		               {
-		               	id: 4,
-		               	name: 'app4',
-		               	status: 'Running',
-		               	owner: 'Daniel',
-		               	created: '4/1/2016'
-		               }
-		           ];
+		$scope.apps = [];
 		
 		$scope.getAction = function (status) {
 			if (status === 'Running') {
@@ -104,5 +75,10 @@ angular.module("UiIntro", [])
 			    year : 'numeric'
 			}).split(' ').join('-');
 		};
+		
+		(function init() {
+	        $scope.refreshApps();
+	    })();
+		
 	}
 );
