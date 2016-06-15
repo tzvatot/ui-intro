cdffc7cf49e7972184611c81455e446905048c76
elad <elad.tabak@ravellosystems.com>
2016-06-15 13:37:07 +0300

    add support for refresh application list from server using

--- js/ng-tutorial.js
+++ js/ng-tutorial.js
@@ -1,5 +1,5 @@
 angular.module("UiIntro", [])
-	.controller("AppsController", function($scope) {
+	.controller("AppsController", function($scope, $http) {
 		$scope.apps = [
 		               {
 		               	id: 1,
@@ -43,6 +43,66 @@ angular.module("UiIntro", [])
 		
 		$scope.getStatusClass = function (status) {
 			return 'status-' + status.toLowerCase();;
+		};
+		
+		$scope.refreshApps = function() {
+			$http({
+				method: 'GET',
+				url: '/services/applications',
+				username : 'ravello@ravello.com',
+				password : 'ravello'
+			}).then(function successCallback(response) {
+				$scope.apps = $scope.parseApps(response.data);
+			}, function errorCallback(response) {
+				console.log(response);
+			});
+		};
+		
+		$scope.parseApps = function (fullApps) {
+			var parsedApps = [];
+			for (i = 0; i < fullApps.length; i++) {
+				var fullApp = fullApps[i];
+				var app = { 
+						name: $scope.formatName(fullApp.name), 
+						owner: fullApp.owner,
+						created: $scope.formatDate(fullApp.creationTime),
+						status: $scope.getStatusFromApp(fullApp),
+						id: fullApp.id
+				};
+				parsedApps.push(app);
+			}
+			return parsedApps;
+		};
+		
+		$scope.getStatusFromApp = function (fullApp) {
+			var deployment = fullApp.deployment;
+			if (typeof deployment != 'undefined') {
+				var totalErrorVms = deployment.totalErrorVms;
+				if (totalErrorVms > 0) {
+					return 'Error';
+				} else {
+					var totalActiveVms = deployment.totalActiveVms;
+					if (totalActiveVms > 0) {
+						return 'Running';
+					} else {
+						return 'Stopped';
+					}
+				}
+			} else {
+				return 'Stopped';
+			}
+		};
+		
+		$scope.formatName = function (name) {
+			return name.replace(new RegExp('<', 'g'), '&lt;').replace(new RegExp('>', 'g'), '&gt;');
+		};
+
+		$scope.formatDate = function (epoch) {
+			return new Date(epoch).toLocaleDateString('en-GB', {  
+			    day : 'numeric',
+			    month : 'short',
+			    year : 'numeric'
+			}).split(' ').join('-');
 		};
 	}
 );
