75f809341c3dfc17cfcf07c0d159e81299b1d971
elad <elad.tabak@ravellosystems.com>
2016-06-15 12:54:05 +0300

    add angular controller and use the applications in the html using ng-repeat

---
+++ js/ng-tutorial.js
@@ -0,0 +1,34 @@
+angular.module("UiIntro", [])
+	.controller("AppsController", function($scope) {
+		$scope.apps = [
+		               {
+		               	id: 1,
+		               	name: 'app1',
+		               	status: 'Running',
+		               	owner: 'Elad',
+		               	created: '1/1/2016'
+		               },
+		               {
+		               	id: 2,
+		               	name: 'app2',
+		               	status: 'Stopped',
+		               	owner: 'Yaniv',
+		               	created: '2/1/2016'
+		               },
+		               {
+		               	id: 3,
+		               	name: 'app3',
+		               	status: 'Error',
+		               	owner: 'Noam',
+		               	created: '3/1/2016'
+		               },
+		               {
+		               	id: 4,
+		               	name: 'app4',
+		               	status: 'Running',
+		               	owner: 'Daniel',
+		               	created: '4/1/2016'
+		               }
+		           ];
+	}
+);
