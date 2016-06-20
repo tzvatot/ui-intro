6bdb7950bfc8f69bae0700866cef5c88eccaa726
elad <elad.tabak@ravellosystems.com>
2016-06-20 11:39:50 +0300

    fix permissions for fetching server data, use basic auth header + WIP for supporting app rename

--- js/ng-tutorial.js
+++ js/ng-tutorial.js
@@ -3,6 +3,7 @@ var uiIntroApp = angular.module("UiIntro", []);
 uiIntroApp.controller("AppsController", function($scope, $http) {
 		$scope.apps = [];
 		$scope.vms = [];
+		$scope.newAppName = '';
 		
 		$scope.getAction = function (status) {
 			if (status === 'Running') {
@@ -25,9 +26,7 @@ uiIntroApp.controller("AppsController", function($scope, $http) {
 			$http({
 				method: 'GET',
 				url: '/services/applications',
-				async: false,
-				username : 'ravello@ravello.com',
-				password : 'ravello'
+				async: false
 			}).then(function successCallback(response) {
 				$scope.apps = $scope.parseApps(response.data);
 			}, function errorCallback(response) {
@@ -86,39 +85,43 @@ uiIntroApp.controller("AppsController", function($scope, $http) {
 		
 		$scope.showAppInfo = function(app) {
 			$scope.isAppInfoVisible = true;
+//			$scope.closeRenameAppDialog();
 			$scope.selectedApp = app;
 			$scope.refreshVms(app.id);
 		};
 		
 		$scope.refreshVms = function(appId) {
 			$scope.vms = [];
+			$scope.getUpdatedApp(appId, function(fullApp) {
+				$scope.vms = $scope.parseVms(fullApp);
+			});
+		};
+		
+		$scope.getUpdatedApp = function(appId, callback) {
 			$http({
 				method: 'GET',
 				url: '/services/applications/' + appId,
-				async: false,
-				username : 'ravello@ravello.com',
-				password : 'ravello'
 			}).then(function successCallback(response) {
-				$scope.vms = $scope.parseVms(response.data);
+				callback(response.data);
 			}, function errorCallback(response) {
 				console.log(response);
+				throw response;
 			});
 		};
 		
 		$scope.parseVms = function(fullApp) {
-			var parseVms = [];
+			var parsedVms = [];
 			if (fullApp.deployment && fullApp.deployment.vms) {
 				var vmList = fullApp.deployment.vms;
 				for (i = 0; i < vmList.length; i++) {
 					var fullVm = vmList[i];
-					console.log('publish time:', fullVm.firstTimePublished);
 					var vm = { 
 							name: $scope.formatName(fullVm.name), 
 							publishTime: $scope.formatDate(parseInt(fullVm.firstTimePublished)),
 							status: $scope.getStatusFromVm(fullVm),
 							id: fullVm.id
 					};
-					parseVms.push(vm);
+					parsedVms.push(vm);
 				}
 			}
 			if (fullApp.design && fullApp.design.vms) {
@@ -131,10 +134,10 @@ uiIntroApp.controller("AppsController", function($scope, $http) {
 							status: 'Draft',
 							id: fullVm.id
 					};
-					parseVms.push(vm);
+					parsedVms.push(vm);
 				}
 			}
-			return parseVms;
+			return parsedVms;
 		}
 		
 		$scope.getStatusFromVm = function(fullVm) {
@@ -155,7 +158,42 @@ uiIntroApp.controller("AppsController", function($scope, $http) {
 			}
 		};
 		
+		$scope.showRenameAppDialog = function(app) {
+			$scope.openRenameAppDialog();
+			$scope.selectedApp = app;
+		};
+		
+		$scope.openRenameAppDialog = function() {
+			$('.app-rename-dialog').dialog("open");
+		};
+		
+		$scope.closeRenameAppDialog = function() {
+			$('.app-rename-dialog').dialog("close");
+		};
+		
+		$scope.renameApp = function(newAppName) {
+			$scope.getUpdatedApp($scope.selectedApp.id, function(fullApp){
+				fullApp.name = newAppName;
+				$scope.updateApp(fullApp);
+			});
+		};
+		
+		$scope.updateApp = function(fullApp) {
+			$http({
+				method: 'PUT',
+				url: '/services/applications/' + app.id,
+				async: false,
+				data: JSON.stringify(fullApp),
+				dataType : 'json'
+			}).then(function successCallback(response) {
+				$scope.refreshApps();
+			}, function errorCallback(response) {
+				console.log(response);
+			});
+		};
+		
 		function init() {
+			$http.defaults.headers.common['Authorization'] = 'Basic cmF2ZWxsb0ByYXZlbGxvLmNvbTpyYXZlbGxv'; 
 	        $scope.refreshApps();
 	    }
 		
